fd4071cdbb1b3d1e80678e726c2a8c932b87d978
rubix5d <tanitolorunplays@gmail.com>
2024-09-29 17:58:14 +0100

    bitwise operators

---
+++ bit.py
@@ -0,0 +1,23 @@
+#AND returns 1 if both bits are 1
+print(7 & 4)
+print(bin(10))
+
+#OR RETURNS 1 IF one of the bits are 1
+print(7 | 15)
+
+#XOR returns 1 when both bits are different
+print(7 ^ 15)
+
+#ASSIGNMENT
+a = 3
+b = 7
+c = 8
+
+and1 = (a & b)
+and2 = (c & b)
+or1 =  (b | c)
+and3 = (or1 & and2)
+q = (and3 | and1)
+
+print("Value for q is", bin(q))
+
