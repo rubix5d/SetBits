482600310162b35bb8332351d62bb8ffa8593df7
rubix5d <tanitolorunplays@gmail.com>
2024-09-29 16:11:14 +0100

    Reverse Binary

--- ass.py
+++ ass.py
@@ -1,18 +1,23 @@
 num = int(input("Write any binary number: "))
-str_num = str(num)
 digits = [int(x) for x in str(num)]
 digits.reverse()
 print(digits)
 
 def firststbit():
     global num
-    global str_num
     count = 1
     for i in digits:
         if i & 1 == 0:
             count += 1
         elif i & 1 == 1:
-            print(count)
+            if count == 3:
+                print(f"It is at the {count}rd position")
+            elif count == 2:
+                print(f"It is at the {count}nd position")
+            elif count == 1:
+                print(f"It is at the {count}st position")
+            else:
+                print(f"It is at the {count}th position")
             break
-
+            
 firststbit()
