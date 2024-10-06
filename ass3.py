e4037af2e4274fb45c276718f32b915941f3052b
rubix5d <tanitolorunplays@gmail.com>
2024-10-06 18:00:20 +0100

    slicing strings

--- ass3.py
+++ ass3.py
@@ -1,5 +1,6 @@
 str1 = input("Enter a sentence:  ")
+list_1 = []
 for i in range(len(str1)):
     for j in range(i+1,len(str1) +1):
-        print(str1[i:j],end=" ")
-    print()
+        list_1.append(str1[i:j])
+print(list_1)
